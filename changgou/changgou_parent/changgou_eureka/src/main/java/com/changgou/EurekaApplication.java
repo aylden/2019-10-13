e321beb02f6beb6619e3c51c33a21f7f84018906
aylden <xiangshuangzeng@163.com>
2019-10-13 15:35:55 +0800

    second commit

--- changgou/changgou_parent/changgou_eureka/src/main/java/com/changgou/EurekaApplication.java
+++ changgou/changgou_parent/changgou_eureka/src/main/java/com/changgou/EurekaApplication.java
@@ -10,5 +10,6 @@ public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
         System.out.println("修改后");
+        System.out.println("二次修改");
     }
 }
